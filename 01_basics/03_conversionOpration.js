8ed398fa3f2287214e01e352c298460d289e35cc
Saurav Shandilya <108254690+shandilyasaurav405@users.noreply.github.com>
2023-07-16 18:02:38 +0000

    Conversion of operation

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,54 @@
+let score = true
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof  valueInNumber)
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc" => NaN
+//true => 1; false =>0
+
+
+let isLoggedIn = 1
+  let booleanIsloggedIn = Boolean(isLoggedIn)
+  console.log(booleanIsloggedIn);
+
+  //1 => true
+  //0=> false
+  //"" => false
+  //"Saurav" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//*************************Operations*************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "saurav"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" +2 +2);
+// console.log(1 +2 + "2");
+
+console.log( +true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1 =num2=num3= 2 + 2
+
